d54d9890130506714867466d6f656ca62738acad
Philip Haupt <philip.haupt@gmail.com>
2019-05-30 15:19:40 +0100

    add pco analysis and plot

--- scripts/data_trans4mation.R
+++ scripts/data_trans4mation.R
@@ -23,3 +23,11 @@ dist <- vegdist(counts, method = "Jaccard", binary = TRUE)
 #similarity between zones
 simp <- simper(counts, env_data$exposure)
 summary(simp)
+
+# principal coordinates
+pco_dat <- wcmdscale(dist, k = 6)
+plot(pco_dat, choices=1:2, scaling="species", col = env_data$exposure, pch = 15, cex=2)
+plot(pco_dat, choices=1:2, scaling="species", col = as.factor(env_data$shore_zones), pch = 16, cex=2)
+
+# are the groups different between 
+adonis2(counts ~ shore_zones + exposure, data = env_data)
