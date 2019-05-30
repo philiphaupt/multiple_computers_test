dfaf74fe7825f61f84681530a2c7f663904326e2
Philip Haupt <philip.haupt@gmail.com>
2019-05-30 15:32:19 +0100

    add environmental variables fit

--- scripts/data_trans4mation.R
+++ scripts/data_trans4mation.R
@@ -11,13 +11,13 @@ counts <- decostand(counts, "pa")
 
 
 #species accumulation curve
-sp_accum <- vegan::specaccum(count_data, "random", permutations = 1000)
+sp_accum <- vegan::specaccum(counts, "random", permutations = 1000)
 plot(sp_accum)
 
 
 #distance Jaccard for presence absence
 
-dist <- vegdist(counts, method = "Jaccard", binary = TRUE)
+dist <- vegdist(counts, method = "jaccard", binary = TRUE)
 
 
 #similarity between zones
@@ -28,6 +28,8 @@ summary(simp)
 pco_dat <- wcmdscale(dist, k = 6)
 plot(pco_dat, choices=1:2, scaling="species", col = env_data$exposure, pch = 15, cex=2)
 plot(pco_dat, choices=1:2, scaling="species", col = as.factor(env_data$shore_zones), pch = 16, cex=2)
+ef <- envfit(pco_dat ~ dist_pier,data = env_data)
+plot(ef,  bg = rgb(1, 1, 1, 0.5))
 
 # are the groups different between 
-adonis2(counts ~ shore_zones + exposure, data = env_data)
+adonis2(counts ~ shore_zones + exposure + dist_pier, data = env_data)
