f423b60f56d0e23d54615aeed97fab3340026e82
Philip Haupt <philip.haupt@gmail.com>
2019-06-03 16:28:02 +0100

    analysis_updated to reflect new data naming

--- scripts/data_trans4mation.R
+++ scripts/data_trans4mation.R
@@ -1,34 +1,35 @@
 library(vegan)
+library(tidyverse)
 
 #read data
-counts <- read_rds("./count_data.RDS")
+comm_data <- read_rds("./comm_data.RDS")
 env_data <- read_rds("./env_data.RDS")
 
 
 # transform - if count data, e.g. log transform
-counts <- decostand(counts, "pa")
-
+pres_abs_dat <- decostand(comm_data, "pa")
+comm_data_log <- decostand(comm_data, "hell")
 
 
 #species accumulation curve
-sp_accum <- vegan::specaccum(counts, "random", permutations = 1000)
+sp_accum <- vegan::specaccum(comm_data, "random", permutations = 1000)
 plot(sp_accum)
 
 
 #distance Jaccard for presence absence
-dist <- vegdist(counts, method = "jaccard", binary = TRUE)
+dist <- vegdist(comm_data_log, method = "altGower", binary = TRUE)
 
 
 #similarity between zones
-simp <- simper(counts, env_data$exposure)
+simp <- simper(comm_data, env_data$shore_zones)
 summary(simp)
 
 # principal coordinates
 pco_dat <- wcmdscale(dist, k = 6)
 plot(pco_dat, choices=1:2, scaling="species", col = env_data$exposure, pch = 15, cex=2)
 plot(pco_dat, choices=1:2, scaling="species", col = as.factor(env_data$shore_zones), pch = 16, cex=2)
-ef <- envfit(pco_dat ~ dist_pier,data = env_data)
+ef <- envfit(pco_dat ~ distance_from_pier,data = env_data)
 plot(ef,  bg = rgb(1, 1, 1, 0.5))
 
 # are the groups different between 
-adonis2(counts ~ shore_zones + exposure + dist_pier, data = env_data)
+adonis2(comm_data ~ shore_zones + exposure + dist_pier, data = env_data)
