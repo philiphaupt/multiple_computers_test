303a840eb8e9853e2ba5614435f606d59ed35976
Philip Haupt <philip.haupt@gmail.com>
2019-06-03 16:27:46 +0100

    informed random community data sampling added

--- scripts/random_data_generation.R
+++ scripts/random_data_generation.R
@@ -1,5 +1,3 @@
-
-
 #some Code
 rm(list=ls())
 
@@ -10,17 +8,7 @@ library(tidyverse)
 
 
 
-#data.frame(matrix(rep(c(1,2,3,4,5,6,7,4,5,6,3,4),3), ncol = 3, nrow = 12))
-
-#generate some random data
-#x <- data.frame(matrix(rep(sample(0:100, 1000, replace = T),30), ncol = 30, nrow = 12))
-
-x <- data.frame(matrix(rbinom(10000, 2, 0.35), ncol = 12, nrow = 9))
-
-#randomly selected count data
-counts <- x %>% sample_n(9)
-
-#species list
+# species list
 species <- c("Patella vulgata", 
              "Littora obtusata", 
              "Semibalanus balanoides",
@@ -33,25 +21,116 @@ species <- c("Patella vulgata",
              "Littorina saxatilis", 
              "Carcineas maenas",
              "Lipophrys pholis" )
-transects <- c(1:9)#c("1", "2", "3")
-rownames(counts) <- transects       
-colnames(counts) <- species
 
-rm(x)
+# random presence absence data set
+#very abundant
+very_abundant <- rbinom(10000, 100, 0.6)
+#abundant_species <- data.frame(matrix(rbinom(10000, 100, 0.35), ncol = 1, nrow = 10000))
+abundant_species <- rbinom(10000, 100, 0.35)
+#common_species <- data.frame(matrix(rbinom(10000, 10, 0.35), ncol = 1, nrow = 10000))
+common_species <- rbinom(10000, 10, 0.5)
+#rare_species <- data.frame(matrix(rbinom(10000, 1, 0.35), ncol = 1, nrow = 10000))
+rare_species <- rbinom(10000, 1, 0.25)
+#very rare
+very_rare <- rbinom(10000, 1, 0.1)        
 
 
-env_data <- data.frame(matrix(nrow = 9, ncol = 3))
-shore_zones <- rep(c("upper", "middle", "lower"),3)
-exposure <- data.frame(matrix(c(rep(c("covered", "exposed"),3),"exposed","exposed","exposed")), row.names = NULL) %>%
-        sample_n(9) 
-names(exposure) <- c("exposure")       
+#percent_cover <- data.frame(matrix(rbinom(10000, 4, 0.35), ncol = 12, nrow = 10000))
+
+d_vabun <- density(very_abundant)
+d_abun <- density(abundant_species)
+d_comm <- density(common_species)
+d_rare <- density(rare_species)
+d_vrare <- density(very_rare)
+
+
+plot(d_rare, col = "blue", xlim = c(0,40))
+lines(d_comm, col = "green")
+lines(d_abun, col = "red")
+lines(d_vrare, col= "black")
+lines(d_vabun, col = "grey")
+#choose type of community
+#x <- rare_species
+
+# randomly selected 27 samples from the randomly generated data above data
+#pres_abs_dat <- x %>% sample_n(27)
+
+# name community matrix
+comm_data <- data.frame(matrix(nrow = 27, ncol = 12))
+colnames(comm_data) <- species
+
+# populate the data
+
+#Rare species
+#comm_data$`Patella vulgata` <- sample_n(rare_species, 27)
+comm_data[1] <- c(sample(common_species, 9, replace = T), sample(rare_species, 9, replace = T), sample(very_rare, 9, replace = T))
+#comm_data$`Crepidula fornicata` <- sample_n(rare_species, 27)
+comm_data[6] <- c(sample(very_rare, 9, replace = T), sample(rare_species, 9, replace = T), sample(rare_species, 9, replace = T))
+#comm_data$`Anemonia viridis` <- sample_n(rare_species, 27)
+comm_data[7] <- c(sample(very_rare, 9, replace = T), sample(common_species, 9, replace = T), sample(common_species, 9, replace = T))
+#comm_data$`Mytilus edulis` <- sample_n(rare_species, 27)
+comm_data[8] <- c(sample(very_rare, 9, replace = T), sample(very_rare, 9, replace = T), sample(rare_species, 9, replace = T))
+#comm_data$`Ostrea edulis` <- sample_n(rare_species, 27)
+comm_data[9] <- c(sample(very_rare, 9, replace = T), sample(very_rare, 9, replace = T), sample(rare_species, 9, replace = T))
+
+# Common species
+#comm_data$`Littora obtusata` <- sample_n(common_species, 27)
+comm_data[2] <- c(sample(common_species, 27, replace = T))
+#comm_data$`Littorina saxatilis` <- sample_n(common_species, 27)
+comm_data[10] <- c(sample(rare_species, 9, replace = T), sample(common_species, 9, replace = T), sample(common_species, 9, replace = T))
+#comm_data$`Fucus serratus` <- sample_n(common_species, 27)
+comm_data[4] <- c(sample(common_species, 9, replace = T), sample(rare_species, 9, replace = T), sample(abundant_species, 9, replace = T))
+#comm_data$`Fucus spiralis` <- sample_n(common_species, 27)
+comm_data[5] <- c(sample(rare_species, 9, replace = T), sample(very_rare, 9, replace = T), sample(common_species, 9, replace = T))
+#comm_data$`Carcineas maenas`
+comm_data[11] <- c(sample(rare_species, 9, replace = T), sample(common_species, 9, replace = T), sample(common_species, 9, replace = T))
+
+# Abundant species
+#comm_data$`Semibalanus balanoides` <- sample_n(abundant_species, 27)
+comm_data[3] <- c(sample(abundant_species, 9, replace = T), sample(very_abundant, 9, replace = T), sample(rare_species, 9, replace = T))
+#comm_data$`Lipophrys pholis`
+comm_data[12] <- c(sample(very_rare, 9, replace = T), sample(very_rare, 9, replace = T), sample(rare_species, 9, replace = T))
+
+
+#sample id
+sample_id <- seq(1,27,1)
+row.names(comm_data) <- sample_id # rename our samples according to their sample ids.
+
+write_rds(comm_data, "./comm_data.RDS")     
+
+#------------------------
+#env data
+#env_data <- data.frame(matrix(nrow = 9, ncol = 3))
+
+#transects
+transects <- c(1:9)
+
+#env data: distance from pier
 dist_pier <-seq(3,27, by = 3)
 
-env_data[1] <- shore_zones
-env_data[2] <- exposure
-env_data[3] <- dist_pier
-names(env_data) <- c("shore_zones", "exposure")
+#populate env_data
+#env_data[1] <- shore_zones
+#env_data[2] <- dist_pier
 
-write_rds(counts, "./count_data.RDS")
+#shore zones
+shore_zones <- c("upper", "middle", "lower")
+sample_station <- expand.grid(transects, shore_zones) #makes all possible combinations
+sample_station_2 <- expand.grid(dist_pier, shore_zones)
+exposure <- data.frame(matrix(rep(c(rep(c("covered", "exposed"),3),"exposed","exposed","exposed"),3))) %>%
+        sample_n(27) 
+names(exposure) <- c("exposure") 
+
+env_data <- bind_cols(sample_station, sample_station_2, exposure)
+env_data <- env_data[,-4]
+names(env_data) <- c("transect", "shore_zones", "distance_from_pier", "exposure")
+env_data <- env_data %>% arrange(transect)
+row.names(env_data) <- sample_id
+
+
+#housekeeping
+rm(d_abun, d_comm, d_rare, exposure, sample_station, sample_station_2, dist_pier, shore_zones, transects)
+
+
+
 write_rds(env_data, "./env_data.RDS")
 
